fe286a00880c229884a55050f09ec7f8c036aaa9
Prince Chime <princechime@Princes-MBP.mynet>
2025-07-22 17:09:05 +0100

    Added terraform config files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Deploy_to_Azure
+
+### Steps to deploy to azure
+
+In this repo, we shall be learning how to deploy to Azure
---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-storage-demo-rg"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfstorageaccountdemo123" # Must be globally unique
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
