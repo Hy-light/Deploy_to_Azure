af9ca6c4aeaccf0db910d3b5529eb9e4c6b25500
Prince Ugochukwu Chime <48451931+Hy-light@users.noreply.github.com>
2025-07-26 12:31:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "tf-storage-demo-rg"
+  name     = "tf-storage-demo-rg-02"
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "tfstorageaccountdemo123" # Must be globally unique
+  name                     = "tfstorageaccountdemo1205" # Must be globally unique
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
